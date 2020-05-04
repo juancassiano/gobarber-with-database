0c15a57e781ce8b497bcbb5cbf09505911c59968
Juan Cassiano <juancassiano@hotmail.com>
2020-05-04 16:45:21 -0300

    Relational Entitys

---
+++ src/database/migrations/1588618246381-AlterProviderFieldToProviderId.ts
@@ -0,0 +1,48 @@
+import {
+  MigrationInterface,
+  QueryRunner,
+  TableColumn,
+  TableForeignKey,
+} from 'typeorm';
+
+export default class AlterProviderFieldToProviderId1588618246381
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropColumn('appoitments', 'provider');
+
+    await queryRunner.addColumn(
+      'appoitments',
+      new TableColumn({
+        name: 'provider_id',
+        type: 'uuid',
+        isNullable: true,
+      })
+    );
+
+    await queryRunner.createForeignKey(
+      'appoitments',
+      new TableForeignKey({
+        name: 'Appoitmentprovider',
+        columnNames: ['provider_id'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'users',
+        onDelete: 'SET NULL',
+        onUpdate: 'CASCADE',
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropForeignKey('appoitments', 'Appoitmentprovider');
+
+    await queryRunner.dropColumn('appoitments', 'provider_id');
+
+    await queryRunner.addColumn(
+      'appoiments',
+      new TableColumn({
+        name: 'provider',
+        type: 'varchar',
+      })
+    );
+  }
+}
