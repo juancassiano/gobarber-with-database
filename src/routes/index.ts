0c8620aaad8d0124b5a4cbb0f3518af572c79a6e
Juan Cassiano <juancassiano@hotmail.com>
2020-05-04 19:16:05 -0300

    Authenticate Route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express';
+
 import appoitmentsRouter from './appoitments.routes';
 import usersRouter from './users.Routes';
+import sessionsRouter from './sessions.routes';
 
 const routes = Router();
 
 routes.use('/appoitments', appoitmentsRouter);
+routes.use('/sessions', sessionsRouter);
 routes.use('/users', usersRouter);
 
 export default routes;
